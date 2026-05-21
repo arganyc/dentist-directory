503da5fb71d634a6af2ebbf7e3d42ebcf023e300
kay <arganyc@gmail.com>
2026-05-21 12:17:36 -0400

    Add rewrites for sitemap XML routes

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  async rewrites() {
+    return [
+      { source: '/sitemap-static.xml', destination: '/sitemap-static.xml/route' },
+      { source: '/sitemap-dentists-1.xml', destination: '/sitemap-dentists-1.xml/route' },
+      { source: '/sitemap-dentists-2.xml', destination: '/sitemap-dentists-2.xml/route' },
+      { source: '/sitemap-dentists-3.xml', destination: '/sitemap-dentists-3.xml/route' },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
