91912d01ae44e56983a926120844be470b7e8205
kay <arganyc@gmail.com>
2026-06-30 10:25:27 -0400

    Defer dentist sitemap DB queries to runtime

--- src/app/api/sitemap-dentists-1/route.ts
+++ src/app/api/sitemap-dentists-1/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 import { getSql } from '@/lib/db'
 
 export const revalidate = 86400
+export const dynamic = 'force-dynamic'
 
 export async function GET() {
   const base = 'https://www.usdentistsdirectory.com'
